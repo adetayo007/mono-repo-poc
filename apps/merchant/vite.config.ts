1f15cae47e80afee9a3e023ebb7fe8647caafe2c
Adetayo <adetayo.akinsanya@prophius.com>
2024-10-15 15:28:17 +0100

    fix

--- apps/merchant/vite.config.ts
+++ apps/merchant/vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 8000
+    port: 5173
   },
   preview: {
     host: true,
-    port: 8000
+    port: 5173
   }
 })
